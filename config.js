b986de5a7f9cb97b6d5471e719cc59aed585996c
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-10-20 16:31:38 -0600

    Generic links

--- config.js
+++ config.js
@@ -1,38 +1,45 @@
-(function(){
-angular.module('my-app',[])
+(function () {
+    angular.module('my-app', [])
 
-.controller('MainController',function(){
-    var mc = this;
-    mc.test = "test";
-    mc.mediaObj = [
-    {
-        heading:"Media Item 1",
-        url:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTCP3eFIlapqYHlsUlUMbobPPGRPRz_BkKpcK4wW__DxwHkJEfY9A"
-    },
-     {
-        heading:"Media Item 2",
-        url:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTejcOJcRLdRvxoP24GuDjpboc9kHUXRNmMyhDTaApiYG7rL69euw"
-    },
-     {
-        heading:"Media Item 3",
-        url:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQGgAeNby1wmvqLe-R4kHJB_nk6jR5Tyw-o32OEzcbf5HOioQ4lkA"
-    },
-     {
-        heading:"Media Item 4",
-        url:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQE-ZfZ_kQT58ZdVYObxgJqgftHWaJpsu4jRjyo2zg5gjhGAlyNMQ"
-    },
-    {
-        heading:"Media Item 5",
-        url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw55N4RhLSGGo0JyWANnr_MsGpXBwuTysTauRh5lTdQeNJU5JW"
-    },
-    {
-        heading:"Media Item 6",
-        url:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTn5GiZ8_wqs6LvjdKEcYa8EQPs3fCBD-99Au4h5w5KFucMar4a"
-    },
-    {
-        heading:"Media Item 7",
-        url:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQnMOiet_7ynGIZXMFtL8fGH-nbMEdEPouFICji0F6KYbGEg6UE"
-    }
-]
-})
-}())
+        .controller('MainController', function () {
+            var mc = this;
+            mc.test = "test";
+            mc.mediaObj = [
+                {
+                    heading: "Media Item 1",
+                    url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTCP3eFIlapqYHlsUlUMbobPPGRPRz_BkKpcK4wW__DxwHkJEfY9A",
+                    link: "link.html"
+                },
+                {
+                    heading: "Media Item 2",
+                    url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTejcOJcRLdRvxoP24GuDjpboc9kHUXRNmMyhDTaApiYG7rL69euw",
+                    link: "link.html"
+                },
+                {
+                    heading: "Media Item 3",
+                    url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQGgAeNby1wmvqLe-R4kHJB_nk6jR5Tyw-o32OEzcbf5HOioQ4lkA",
+                    link: "link.html"
+                },
+                {
+                    heading: "Media Item 4",
+                    url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQE-ZfZ_kQT58ZdVYObxgJqgftHWaJpsu4jRjyo2zg5gjhGAlyNMQ",
+                    link: "link.html"
+                },
+                {
+                    heading: "Media Item 5",
+                    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw55N4RhLSGGo0JyWANnr_MsGpXBwuTysTauRh5lTdQeNJU5JW",
+                    link: "link.html"
+                },
+                {
+                    heading: "Media Item 6",
+                    url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTn5GiZ8_wqs6LvjdKEcYa8EQPs3fCBD-99Au4h5w5KFucMar4a",
+                    link: "link.html"
+                },
+                {
+                    heading: "Media Item 7",
+                    url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQnMOiet_7ynGIZXMFtL8fGH-nbMEdEPouFICji0F6KYbGEg6UE",
+                    link: "link.html"
+                }
+            ]
+        })
+} ())
